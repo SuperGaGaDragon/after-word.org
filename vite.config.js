46dfc8357a7b423f8b7cd8326fbda4ec9adc744d
catadragon <steinitzchess2311web@users.noreply.github.com>
2026-01-15 23:58:26 -0500

    rebuild vite

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite';
+import { resolve } from 'path';
 
 export default defineConfig({
   root: 'src',
   build: {
     outDir: '../dist',
-    emptyOutDir: true
+    emptyOutDir: true,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/index.html'),
+        login: resolve(__dirname, 'src/login/index.html'),
+        signup: resolve(__dirname, 'src/signup/index.html'),
+        account: resolve(__dirname, 'src/account/index.html')
+      }
+    }
   }
 });
